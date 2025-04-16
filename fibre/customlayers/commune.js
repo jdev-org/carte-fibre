f1f0a984b23cff518a4ac32da0b59f1a65390513
Agath21 <agathe.adam@jdev.fr>
2025-04-16 12:28:24 +0200

    Add communes from geob

--- fibre/customlayers/commune.js
+++ fibre/customlayers/commune.js
@@ -2,12 +2,12 @@
   mviewer.customLayers.commune = {};
   mviewer.customLayers.commune.layer = new ol.layer.Vector({
     source: new ol.source.Vector({
-      url: "apps/fibre/data/commune_simple.geojson",
+      url: "https://geobretagne.fr/geoserver/ign/wfs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GETFEATURE&TYPENAME=commune_metro&&outputFormat=application/json&srsName=EPSG:4326&CQL_FILTER=insee_reg=%2753%27&PROPERTYNAME=nom_com,geom",
       format: new ol.format.GeoJSON(),
     }),
     style: new ol.style.Style({
       stroke: new ol.style.Stroke({
-        color: "rgba(0,0,0,200)",
+        color: "rgba(0,0,0,0)",
         width: 0.1
       }),
       fill: new ol.style.Fill({
